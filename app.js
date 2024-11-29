6f46cd66310bc4916445a0c7639dccb3dcfa11ee
Emma Woelk <emma.woelk@gmail.com>
2024-11-29 13:51:51 -0600

    add logging

--- app.js
+++ app.js
@@ -11,11 +11,14 @@ const userRouter = require("./routes/users");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middleware/error-handler");
 const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const { PORT = 3001 } = process.env;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(requestLogger);
 
 app.use("/", mainRouter);
 app.use("/", clothingItemRouter);
@@ -27,6 +30,8 @@ mongoose
     console.log("Connected to database!");
   })
   .catch(console.error);
+
+app.use(errorLogger);
 
 app.use((req, res, next) => {
   res.status(404).send({ message: "Requested resource not found" });
