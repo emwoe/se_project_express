a1c68fa25f92d1726a89a7e3c5ea4c92e8b3d9f0
Emma Woelk <emma.woelk@gmail.com>
2024-12-01 11:22:00 -0600

    fix validateCardBody

--- middleware/validation.js
+++ middleware/validation.js
@@ -9,18 +9,20 @@ const validateURL = (value, helpers) => {
 };
 
 module.exports.validateCardBody = celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30).messages({
-      "string.min": 'The minimum length of the "name" field is 2',
-      "string.max": 'The maximum length of the "name" field is 30',
-      "string.empty": 'The "name" field must be filled in',
-    }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string().required().min(2).max(30).messages({
+        "string.min": 'The minimum length of the "name" field is 2',
+        "string.max": 'The maximum length of the "name" field is 30',
+        "string.empty": 'The "name" field must be filled in',
+      }),
 
-    imageUrl: Joi.string().required().custom(validateURL).messages({
-      "string.empty": 'The "imageUrl" field must be filled in',
-      "string.uri": 'the "imageUrl" field must be a valid url',
-    }),
-  }),
+      imageUrl: Joi.string().required().custom(validateURL).messages({
+        "string.empty": 'The "imageUrl" field must be filled in',
+        "string.uri": 'the "imageUrl" field must be a valid url',
+      }),
+    })
+    .unknown(true),
 });
 
 module.exports.validateUserBody = celebrate({
